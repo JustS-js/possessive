4781b56b4d005599d606ed39b75ff5b0c44e0d52
Just <grivten@yandex.ru>
2025-09-03 19:31:45 +0700

    port for 1.21.5 from 1.0.4_1.21.4

--- src/client/java/net/just_s/mixin/client/LocalPlayerMixin.java
+++ src/client/java/net/just_s/mixin/client/LocalPlayerMixin.java
@@ -3,6 +3,7 @@ package net.just_s.mixin.client;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.just_s.PossessiveModClient;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
@@ -11,19 +12,25 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Environment(EnvType.CLIENT)
 @Mixin(LocalPlayer.class)
 public class LocalPlayerMixin {
-
-    @Shadow @Final public static Logger LOGGER;
-
     @Inject(method = "sendPosition", at = @At("HEAD"), cancellable = true)
     private void possessive$onSendPosition(CallbackInfo ci) {
         if (PossessiveModClient.cameraHandler.isEnabled()) {
             /*if (PossessiveModClient.cameraHandler.getCamera().onSendPosition()) {
                 ci.cancel();
             }*/
+        }
+    }
+
+    @Inject(method = "isControlledCamera", at = @At("RETURN"), cancellable = true)
+    private void possessive$ensureLocalPlayerSendingMovement(CallbackInfoReturnable<Boolean> cir) {
+        if (PossessiveModClient.cameraHandler.isEnabled()) {
+            boolean isOriginalBodyOfLocalPlayer = Minecraft.getInstance().player == ((LocalPlayer)(Object)this);
+            cir.setReturnValue(cir.getReturnValue() || isOriginalBodyOfLocalPlayer);
         }
     }
 }
